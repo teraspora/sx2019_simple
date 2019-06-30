c4c85f6748f55ec01508f87236fe67fef6a406cf
John Lynch <johnlynch.32768@gmail.com>
2019-06-30 14:05:16 +0100

    Refactored code and added comments; added uniform .

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
-// function get_shader(path) {
-//     fetch(path)
-//         .then(response => response.text())
-//         .then(text => {
-//             return text;
-//         });
-// }
+// Code to read, load, compile, link, run and render
+// (to an HTML5 canvas) a vertex and a fragment shader 
 
+// Get reference to canvas, set dimensions
 const canvas = document.getElementById(`canv0`);
 let [w, h] = [window.innerWidth - 400, window.innerHeight - 200];
 [canvas.width, canvas.height] = [w, h];
 
+// Link canvas to a WebGL context
 const gl = canvas.getContext(`webgl`) || canvas.getContext(`experimental-webgl`);
+
+// Initialise context
 gl.viewport(0, 0, w, h);
 gl.clearColor(0.03, 0, 0.2, 1);
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+// Create some points to send to the vertex shader and set up a buffer for passing them to the GPU
 const triangleVertices = [
     //  x       y       R       G       B   
         0.0,    0.5,    1.0,    1.0,    0.0,
@@ -24,16 +24,26 @@ const triangleVertices = [
 let triangleVertexBufferObject = gl.createBuffer();
 
 let vShaderCode, fShaderCode;   // strings read from user-specified files
-let vertexShader, fragmentShader; 
+let vertexShader, fragmentShader;
+
+// Get refs to the input elements that let user choose the files that contain the shader code
 let vs_input = document.getElementById('vs-code-input');
 let fs_input = document.getElementById('fs-code-input');
 
+// t0 will store the initial time, just before first render
+let t0;
+
+// Set up uniforms
+let uniforms = {};
+uniforms.u_time = {type: 'f', value: 0.0};
+
+// Listen for user choosing the shader files
 for (let input of [vs_input, fs_input]) {
   input.addEventListener('change', handleFileSelect, false);  
 } 
     
-
-let [vs_loaded, fs_loaded] = [false, false];
+// 3 state booleans: Vertex shader loaded? / Fragment6 shader loaded? / User clicked "Pause"? 
+let [vs_loaded, fs_loaded, paused] = [false, false, false];
 
 function handleFileSelect(e0) {
     f = e0.target.files[0];
@@ -45,7 +55,7 @@ function handleFileSelect(e0) {
             if (e0.target === vs_input) {
                 vShaderCode = rdr.result;
                 console.log(`*** Vertex shader source code *** \n\n${vShaderCode}\n`);
-                // Load vertex shader code
+                // Load and compile vertex shader code
                 vertexShader = gl.createShader(gl.VERTEX_SHADER);
                 gl.shaderSource(vertexShader, vShaderCode);
                 gl.compileShader(vertexShader);
@@ -61,7 +71,7 @@ function handleFileSelect(e0) {
             else if (e0.target === fs_input) {
                 fShaderCode = rdr.result;
                 console.log(`*** Fragment shader source code *** \n\n${fShaderCode}\n`);
-                // Load fragment shader code
+                // Load and compile fragment shader code
                 fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
                 gl.shaderSource(fragmentShader, fShaderCode);
                 gl.compileShader(fragmentShader);
@@ -96,6 +106,8 @@ function linkShaders() {
     }
     setupBuffers();
     linkAttributes(program);
+    t0 = Date.now();
+    gl.useProgram(program);
     render(program);  
 }
 
@@ -128,12 +140,19 @@ function linkAttributes(program) {
     gl.enableVertexAttribArray(colourAttribLocation);
 }
 
-
 function render(program) {
-    gl.useProgram(program);
-    gl.drawArrays(
-        gl.TRIANGLES,     // WebGL drawing mode
-        0,            // How many vertices to skip
-        3,            // How many vertices to draw
-    );
+    if (!paused) {
+        uniforms.u_time.value = 0.001 * (Date.now() - t0);
+        // uniforms.u_resolution.value.x = w;
+        // uniforms.u_resolution.value.y = h;           
+        gl.drawArrays(
+            gl.TRIANGLES,   // WebGL drawing mode
+            0,              // How many vertices to skip
+            3,              // How many vertices to draw
+        );
+    }
+}
+
+function reset() {
+    t0 = Date.now();
 }
